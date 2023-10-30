343ddb04e8b332c77aa6751106fd3b0330d41b05
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-30 13:25:08 +0100

    api fonctionnel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // Le setup d'express
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 const app = express();
+
+
+// Dans le cadre d'une API, les donnée du corps d'une requête seront envoyé au format JSON
+// Nous devons donc spécifier à express qu'il devra decoder le req.body au format JSON
+app.use(bodyParser.json());
 
 
 
